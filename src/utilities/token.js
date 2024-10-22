d260e8214f9d531b025a71fe640e68f7f0f10f78
navya2012 <navyapadala1995@gmail.com>
2024-10-22 12:44:32 +0530

    token

--- src/utilities/token.js
+++ src/utilities/token.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken")
 
 const createToken = ({_id, role, email}) => {
     return jwt.sign({_id, role, email}, process.env.JWT_TOKEN ,{
-        expiresIn:"5m"
+        expiresIn:"1d"
     })
 }
 
